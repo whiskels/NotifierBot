67cc9e363acabe2436cfd49905665dad3d569b84
whiskels <kuzmingeo@gmail.com>
2020-11-20 17:14:10 +0300

    update: add "slack" profile

--- src/main/java/com/whiskels/notifier/slack/BirthdayHandler.java
+++ src/main/java/com/whiskels/notifier/slack/BirthdayHandler.java
@@ -3,11 +3,13 @@ package com.whiskels.notifier.slack;
 import com.whiskels.notifier.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Profile({"slack", "test"})
 public class BirthdayHandler extends AbstractSlackHandler {
     @Value("${slack.employee.birthday.webhook}")
     private String webHook;
