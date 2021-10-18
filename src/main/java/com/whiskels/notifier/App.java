ca6c4a156c3df64df3598a9382e588a8d5d090c9
whiskels <kuzmingeo@gmail.com>
2021-10-18 19:49:15 +0300

    temp

--- src/main/java/com/whiskels/notifier/App.java
+++ src/main/java/com/whiskels/notifier/App.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Profile;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties
+@EnableRetry
 @Slf4j
 @Profile("!test")
 public class App {
