1951812dfa47405b746b9756ad89bd165768542b
whiskels <kuzmingeo@gmail.com>
2022-12-04 21:22:27 +0300

    fix: missing generic class name

--- src/main/java/com/whiskels/notifier/external/json/currency/_CurrencyRateBeanConfig.java
+++ src/main/java/com/whiskels/notifier/external/json/currency/_CurrencyRateBeanConfig.java
@@ -3,18 +3,27 @@ package com.whiskels.notifier.external.json.currency;
 import com.whiskels.notifier.external.Loader;
 import com.whiskels.notifier.external.MemoizingReportSupplier;
 import com.whiskels.notifier.external.ReportSupplier;
+import com.whiskels.notifier.external.audit.Audit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
+import java.util.List;
 
+import static com.whiskels.notifier.external.LoaderType.CURRENCY_RATE;
 import static java.util.Collections.singletonList;
 
 @Configuration
 class _CurrencyRateBeanConfig {
     @Bean
     public Loader<CurrencyRate> currencyRateLoader(CurrencyRateFeignClient currencyRateClient) {
-        return () -> singletonList(currencyRateClient.get());
+        return new Loader<CurrencyRate>() {
+            @Override
+            @Audit(loader = CURRENCY_RATE)
+            public List<CurrencyRate> load() {
+                return singletonList(currencyRateClient.get());
+                }
+            };
     }
 
     @Bean
