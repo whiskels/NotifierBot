545e2024b13c72b92ae7cd715f64de2619d7de69
whiskels <kuzmingeo@gmail.com>
2024-07-12 10:34:23 +0300

    T-38: fixed NPE in comparator

--- src/main/java/com/whiskels/notifier/reporting/domain/HasBirthday.java
+++ src/main/java/com/whiskels/notifier/reporting/domain/HasBirthday.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 
 public interface HasBirthday {
     static <T extends HasBirthday> Comparator<T> comparator() {
-        return Comparator.comparing(T::birthday).thenComparing(T::name);
+        return Comparator.comparing(T::birthday, Comparator.nullsLast(Comparator.naturalOrder())).thenComparing(T::name, Comparator.nullsLast(Comparator.naturalOrder()));
     }
 
 
