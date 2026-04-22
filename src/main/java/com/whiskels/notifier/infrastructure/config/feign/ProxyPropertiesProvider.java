e6fff570d238a7ffff7c2e735d579947f4f5402e
whiskels <kuzmingeo@gmail.com>
2026-04-22 14:33:35 +0100

    chore: update proxy config with basic auth

--- src/main/java/com/whiskels/notifier/infrastructure/config/feign/ProxyPropertiesProvider.java
+++ src/main/java/com/whiskels/notifier/infrastructure/config/feign/ProxyPropertiesProvider.java
@@ -2,11 +2,15 @@ package com.whiskels.notifier.infrastructure.config.feign;
 
 
 interface ProxyPropertiesProvider {
+    String getProxyUser();
+
+    String getProxyPassword();
+
+    String getProxyHost();
+
+    int getProxyPort();
+
     String getUser();
 
     String getPassword();
-
-    String getHost();
-
-    int getPort();
 }
